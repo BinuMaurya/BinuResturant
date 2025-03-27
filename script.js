eba650ac05b3dea15d3003b917fa88cf4fd93d1c
BinuMaurya <binumaurya623@gmail.com>
2025-03-28 00:26:10 +0530

    addneew file

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let heroContent = document.querySelector(".hero-content h1");
+    heroContent.innerHTML = "Welcome to BINU RESTAURANT"; // Update text
+});
